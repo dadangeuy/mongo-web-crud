526de98bf925d3dc2735594d8d72dc27ba3d9250
dadangeuy <irsyad.rizaldi97@gmail.com>
2018-11-16 19:21:53 +0700

    List Recipe Pagination

--- src/main/java/com/rizaldi/mongo/web/crud/controller/FoodRecipeController.java
+++ src/main/java/com/rizaldi/mongo/web/crud/controller/FoodRecipeController.java
@@ -1,8 +1,6 @@
 package com.rizaldi.mongo.web.crud.controller;
 
 import com.rizaldi.mongo.web.crud.repository.FoodRecipeRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,8 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class FoodRecipeController {
-    private static final Logger LOG = LoggerFactory.getLogger(FoodRecipeController.class);
-    private static final int MAX_RECIPE_IN_PAGE = 10;
+    private static final int MAX_RECIPE_IN_PAGE = 5;
     private final FoodRecipeRepository repository;
 
     public FoodRecipeController(FoodRecipeRepository repository) {
@@ -30,7 +27,7 @@ public class FoodRecipeController {
         var recipes = repository.findAll(page).getContent();
         model.addAttribute("recipes", recipes);
         model.addAttribute("startIndex", pageNumber * MAX_RECIPE_IN_PAGE + 1);
-        LOG.info("found {} recipes", recipes.size());
+        model.addAttribute("currentPage", pageNumber);
         return "ListRecipe";
     }
 }
