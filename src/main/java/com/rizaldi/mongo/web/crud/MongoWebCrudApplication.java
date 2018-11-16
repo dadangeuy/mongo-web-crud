eb958e1a400bfa9ed85287f95c9d3e41bdbd2f0f
dadangeuy <irsyad.rizaldi97@gmail.com>
2018-11-16 10:54:14 +0700

    Initial Project

---
+++ src/main/java/com/rizaldi/mongo/web/crud/MongoWebCrudApplication.java
@@ -0,0 +1,12 @@
+package com.rizaldi.mongo.web.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWebCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoWebCrudApplication.class, args);
+    }
+}
